1f986ea9c82ff462b719af949ab188280919a7e2
Jerson97 <jersonramirezs97@gmail.com>
2025-11-12 16:17:04 -0500

    Feat: se agrego las clases Pasos y archivo adjunto

--- Tareas/Data/ApplicationDbContext.cs
+++ Tareas/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Tareas.Models.Entities;
 
 namespace Tareas.Data
 {
@@ -7,5 +8,16 @@ namespace Tareas.Data
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Tarea>().Property(t => t.Titulo).HasMaxLength(250).IsRequired();
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+        public DbSet<ArchivoAdjunto> ArchivoAdjuntos { get; set; }
     }
 }
